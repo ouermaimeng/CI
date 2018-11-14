ead0803b4caee60b91436c96ed3cfe398c1bf741
ouermaimeng <414162938@qq.com>
2018-11-14 11:31:10 -0500

    fef

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "shell test"
-
-ssh travis@107.182.188.188 -p 12306 -v exit
+mod 600 ~/.ssh/id_rsa
+echo "shell test" 
+ssh travis@107.182.188.188 -p 12306 -o StrictHostKeyChecking=no exit
